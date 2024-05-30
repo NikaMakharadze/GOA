df7275c99a4431c77cb9778a1a0797511a67ef14
NikaMakharadze <teimurazmakharadze83@gmail.com>
2024-05-30 15:10:34 +0400

    first commit

---
+++ level 3/homework/homework03.py
@@ -0,0 +1,12 @@
+age = 14
+name = "Nika"
+salary = "None"
+learning_programming = True
+favorite_color = "Purple, Black"
+height = 179
+weight = 84
+city = "Tbilisi"
+eye_color = "brown"
+has_pet = True
+
+print(age,  name,  salary,  str(learning_programming),  favorite_color,  height,  weight,  city,  eye_color,  str(has_pet))
