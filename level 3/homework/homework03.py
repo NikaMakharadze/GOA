4057214e0be34f52474da0e619f8260c77b4b73a
NikaMakharadze <teimurazmakharadze83@gmail.com>
2024-05-30 16:19:04 +0400

    level 3

--- level 3/homework/homework03.py
+++ level 3/homework/homework03.py
@@ -1,12 +1,2 @@
-age = 14
-name = "Nika"
-salary = "None"
-learning_programming = True
-favorite_color = "Purple, Black"
-height = 179
-weight = 84
-city = "Tbilisi"
-eye_color = "brown"
-has_pet = True
-
-print(age,  name,  salary,  str(learning_programming),  favorite_color,  height,  weight,  city,  eye_color,  str(has_pet))
+txt = "me mqvia Nikoloz Maxaradze var 14 wlis maqvs gamocdileba mxolod js tan konkretulad discord@13 js minda momavalshi gavxde warmatebuli web developeri"
+print(txt)
