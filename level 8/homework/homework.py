a3ace7c6b5f75413275fa2240bb3d0d6dafaecbe
NikaMakharadze <teimurazmakharadze83@gmail.com>
2024-07-03 19:31:17 +0400

    Level 8

---
+++ level 8/homework/homework.py
@@ -0,0 +1,30 @@
+num = input("Enter your num: ")
+num2 = input("Enter your num2: ")
+print("------------")
+
+print(num > num2)
+print(num < num2)
+print(num == num2)
+print(num != num2)
+print(num >= num2)
+print(num <= num2)
+print("------------")
+
+print(5 < 4)
+print(11 > 1)
+print(10 == 10)
+print(16 != 16)
+print(19 >= 19)
+print(21 <= 21)
+print("------------")
+
+# reassign
+num = 5
+
+result = num > 3
+print(result)
+
+num = 1
+
+result = num > 3
+print(result)
