37dc0c3fe384a62c3ed444f2cca5563496c43095
NikaMakharadze <teimurazmakharadze83@gmail.com>
2024-07-04 23:14:39 +0400

    Level 8

--- level 8/homework/homework.py
+++ level 8/homework/homework.py
@@ -10,12 +10,12 @@ print(num >= num2)
 print(num <= num2)
 print("------------")
 
-print(5 < 4)
-print(11 > 1)
-print(10 == 10)
-print(16 != 16)
-print(19 >= 19)
-print(21 <= 21)
+print(5 < 4) # False
+print(11 > 1) # True
+print(10 == 10) # True
+print(16 != 16) # False
+print(19 >= 19) # True
+print(21 <= 21) # True
 print("------------")
 
 # reassign
